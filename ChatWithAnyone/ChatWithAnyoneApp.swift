1a15cc50003e3cdc445d9b9911a0c66ab6b49123
ryanimgrund <70997057+ryanimgrund@users.noreply.github.com>
2025-07-30 16:18:42 -0400

    Initial Commit

---
+++ ChatWithAnyone/ChatWithAnyoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatWithAnyoneApp.swift
+//  ChatWithAnyone
+//
+//  Created by Ryan Imgrund on 2025-07-30.
+//
+
+import SwiftUI
+
+@main
+struct ChatWithAnyoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatWithAnyoneTests/ChatWithAnyoneTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChatWithAnyoneTests.swift
+//  ChatWithAnyoneTests
+//
+//  Created by Ryan Imgrund on 2025-07-30.
+//
+
+import Testing
+@testable import ChatWithAnyone
+
+struct ChatWithAnyoneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
